81252edee2300821c08bde1994802d88dee53ff4
LaMavia <ts438730@students.mimuw.edu.pl>
2024-01-31 13:48:02 +0100

    update nvim path

--- home-manager/modules/nvim/default.nix
+++ home-manager/modules/nvim/default.nix
@@ -3,5 +3,5 @@ let
   config-derivation = pkgs.callPackage ./derivation.nix { };
 in
 {
-  home.file.".config/nvim-maybe".source = config-derivation;
+  home.file.".config/nvim".source = config-derivation;
 }
