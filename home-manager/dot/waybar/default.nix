b18fbfbe52d4c805fd946aff547e58389f702505
LaMavia <ts438730@students.mimuw.edu.pl>
2024-02-04 12:02:42 +0100

    add waybar idle_inhibitor

--- home-manager/dot/waybar/default.nix
+++ home-manager/dot/waybar/default.nix
@@ -1,5 +1,8 @@
-{...}:
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    font-awesome
+  ];
   home.file.".config/waybar/config".source = ./config;
   home.file.".config/waybar/style.css".source = ./style.css;
 }
