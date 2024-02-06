8e0af938b3b481a4d57b5f2d2f371a82848e2895
LaMavia <ts438730@students.mimuw.edu.pl>
2024-02-06 09:56:26 +0100

    organise waybar themes

--- home-manager/dot/waybar/default.nix
+++ home-manager/dot/waybar/default.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [
     font-awesome
   ];
-  home.file.".config/waybar/config".source = ./config.json5;
-  home.file.".config/waybar/style.css".source = ./style.css;
+  home.file.".config/waybar/config".source = ./themes/own/config.json5;
+  home.file.".config/waybar/style.css".source = ./themes/own/style.css;
 }
