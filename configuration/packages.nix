018b7b93e9b2069dfeab509fd1ed6502cd85c41f
LaMavia <ts438730@students.mimuw.edu.pl>
2024-07-13 01:24:01 +0200

    nsq

--- configuration/packages.nix
+++ configuration/packages.nix
@@ -79,23 +79,4 @@ in
     OVMFFull
     swtpm
   ];
-
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-    gedit
-  ]) ++ (with pkgs.gnome; [
-    cheese # webcam tool
-    gnome-music
-    gnome-terminal
-    epiphany # web browser
-    geary # email reader
-    evince # document viewer
-    gnome-characters
-    totem # video player
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
-  ]);
 }
