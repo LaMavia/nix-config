e3cb78fcc2db2c89ed5b575ce4fe77703b0f05ca
LaMavia <ts438730@students.mimuw.edu.pl>
2024-07-13 01:11:49 +0200

    nsq

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ 
+        ./hardware-configuration.nix 
         ./configuration.nix 
         ./configuration/systemd.nix 
         ./configuration/packages.nix 
