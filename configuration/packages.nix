0b82c2a3d864856824f121b0525963b0d9895af8
LaMavia <ts438730@students.mimuw.edu.pl>
2024-07-13 02:46:52 +0200

    nsq

--- configuration/packages.nix
+++ configuration/packages.nix
@@ -11,6 +11,28 @@ let
       systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
     '';
   };
+
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        export gnome_schema=org.gnome.desktop.interface
+        export GTK_THEME=nordic
+        echo "$XDG_DATA_DIRS | $gnome_schema"
+        gsettings set $gnome_schema gtk-theme 'Dracula'
+        gsettings set org.gnome.desktop.wm.preferences theme 'Nordic'
+        # gsettings set $gnome_schema icon-theme 'Adwaita'
+        gsettings set $gnome_schema cursor-theme 'Volantes Cursors'
+      '';
+  };
+
 in
 {
   environment.systemPackages = with pkgs; [
@@ -23,6 +45,7 @@ in
     brave
     sway
     dbus-sway-environment
+    configure-gtk
     kitty
     wayland
     xdg-utils
@@ -42,5 +65,6 @@ in
     brightnessctl
     pulseaudio
     home-manager
+    gh
   ];
 }
