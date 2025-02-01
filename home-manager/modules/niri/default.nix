cc1ba7a3b0ba971ff0a29946ed3b84b9cc8dd991
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-02-01 14:13:42 +0100

    nsq

--- home-manager/modules/niri/default.nix
+++ home-manager/modules/niri/default.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 {
-  programs.niri = {
-    enable = true;
-    config = ./niri.config.kdl; 
-  };
-
   home.packages = with pkgs; [
     swww
   ];
+
+  home.file."~/.scripts" = {
+    source = ./scripts;
+    recursive = true;
+  };
+
+  programs.niri = {
+    config = builtins.readFile ./niri.config.kdl;
+  };
+
 }
