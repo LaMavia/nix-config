a1adf6ba37b342148a3127fcc266921429290660
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-06-20 17:47:35 +0200

    nsq

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -9,21 +9,23 @@
       direnv hook fish | source
       if test "$TERM" = "xterm-kitty";
         alias kssh="TERM=xterm-256color ssh"
+        set -U __done_kitty_remote_control 1
+        set -U __done_kitty_remote_control_password "kitty-rc-password"
       end
     '';
 
     plugins = [
       # Enable a plugin (here grc for colorized command output) from nixpkgs
       { name = "grc"; src = pkgs.fishPlugins.grc.src; }
-      { 
-        name = "loadenv";  
+      {
+        name = "loadenv";
         src = pkgs.fetchFromGitHub {
           owner = "berk-karaal";
           repo = "loadenv.fish";
           rev = "e5ad1b3e8cf779bd897a5fa4c0dc55a920b01ed7";
           sha256 = "RyGjJ8NxTqEr9MW7hnrTlry6fW+IF4el1IPUh7WIwxU=";
         };
-      } 
+      }
       # Manually packaging and enable a plugin
       {
         name = "z";
@@ -34,6 +36,10 @@
           sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
         };
       }
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
     ];
   };
 
@@ -41,9 +47,7 @@
     starship
     grc
     fish
-    # (mommy.override {
-    #   mommySettings = {};
-    # })
+    jq
   ];
 
   home.file.".config/starship.toml".source = ./starship.toml;
