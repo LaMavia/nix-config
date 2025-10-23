7ceb1ed87e745d669047ab2f46f187a5be435947
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-10-23 23:32:01 +0200

    fix config

--- home-manager/modules/nvim/default.nix
+++ home-manager/modules/nvim/default.nix
@@ -1,10 +1,11 @@
 { pkgs, config, ... }:
-let 
- path = "/home/mavia/.nixos/home-manager/modules/nvim/config";
-in {
- home.file.".config/nvim" = {
-  source = builtins.trace "nvim config path: ${path}" (config.lib.file.mkOutOfStoreSymlink "${path}");
- };
+let
+  path = "/home/mavia/.nixos/home-manager/modules/nvim/config_";
+in
+{
+  home.file.".config/nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${path}";
+  };
 
- home.packages = import ./packages.nix { pkgs=pkgs; };
+  home.packages = import ./packages.nix { pkgs = pkgs; };
 }
