f616aea6a62f77935aa73bee178fa9fceb942338
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-10-23 22:10:27 +0200

    nsq

--- flake.nix
+++ flake.nix
@@ -5,44 +5,47 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # nixvim = {
-    #   url = "github:nix-community/nixvim";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    nixvim = {
+      url = "path:./home-manager/modules/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     niri = {
       url = "github:sodiboo/niri-flake";
     };
     # Neve.url = "github:redyf/Neve";
   };
   outputs = { self, nixpkgs, home-manager, niri, ... } @ inputs: {
+    overlays.additions = final: prev: {
+      nixvim = inputs.nixvim.packages.${prev.system}.default;
+    };
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {
         inherit inputs;
       };
-      modules = [ 
-        ./hardware-configuration.nix 
-        ./configuration.nix 
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
         ./hardware-override.nix
-        ./configuration/systemd.nix 
-        ./configuration/packages.nix 
-        ./configuration/programs.nix 
-        ./configuration/services.nix 
+        ./configuration/systemd.nix
+        ./configuration/packages.nix
+        ./configuration/programs.nix
+        ./configuration/services.nix
         ./configuration/hardware.nix
         ./configuration/virtualisation/docker.nix
         ./configuration/virtualisation/wayland.nix
         niri.nixosModules.niri
         home-manager.nixosModules.home-manager
-          {
-            nixpkgs.overlays = [
-              niri.overlays.niri
-            ];
-            # home-manager.backupFileExtension = "backup";
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mavia = import ./home-manager/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs;};
-          }
+        {
+          nixpkgs.overlays = [
+            niri.overlays.niri
+          ];
+          # home-manager.backupFileExtension = "backup";
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.mavia = import ./home-manager/home.nix;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
       ];
     };
   };
--- home-manager/modules/nixvim/default.nix
+++ home-manager/modules/nixvim/default.nix
@@ -1,11 +1,27 @@
-{ pkgs, lib, inputs, ... }:
+_:
+
 {
-  programs.nixvim = {
-    enable = true;
-    imports = [ 
-      inputs.Neve.nixvimModule 
-    ];
-    colorschemes.catppuccin.enable = lib.mkForce false;
-    colorschemes.nord.enable = true;
+  imports = [
+    ./config
+    ./plugins
+    ./utils
+  ];
+
+  config = {
+    # Use <Space> as leader key
+    globals.mapleader = " ";
+
+    # Set 'vi' and 'vim' aliases to nixvim
+    viAlias = true;
+    vimAlias = true;
+
+    # Setup clipboard support
+    clipboard = {
+      # Use xsel as clipboard provider
+      providers.xsel.enable = true;
+
+      # Sync system clipboard
+      register = "unnamedplus";
+    };
   };
 }
